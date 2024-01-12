222d0cb804dcae57321080386f62b7168b12f11d
farneses <latriasouls@protonmail.com>
2024-01-12 00:26:31 -0300

    better install

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 mv .config /home/farnese/ &&
 mv .local /home/farnese/ &&
-mv wal /home/farnese/ 
+mv wal /home/farnese/ &&
+wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh &&
+
