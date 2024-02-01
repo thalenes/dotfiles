6969fbcf95e4453b3eceee86bb6ac3861c03517d
farneses <latriasouls@protonmail.com>
2024-02-01 02:31:11 -0300

    added theme and wallpapers

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+rm -rf /home/farnese/.config &&
+rm -rf /home/farnese/.local &&
+
 mv .config /home/farnese/ &&
 mv .local /home/farnese/ &&
 mv wal /home/farnese/ &&
-wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh &&
+wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh 
 
