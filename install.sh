5a942a432f19590d5559cf8738c40b00aaeec7fe
twosich <latriasouls@protonmail.com>
2024-09-14 02:11:30 -0300

    edit script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 sudo pacman -S git wget curl &&
 git clone https://aur.archlinux.org/yay-bin && cd yay-bin && makepkg -si &&
-sudo pacman -S hyprland hyprshot swaybg swaync lxsession-gtk3 alacritty rofi waybar thunar xdg-desktop-portal-hyprland xdg-desktop-portal-gtk && 
+sudo pacman -S hyprland hyprshot swaybg swaync lxsession-gtk3 alacritty rofi waybar thunar xdg-desktop-portal-hyprland xdg-desktop-portal-gtk helix && 
 sudo pacman -S ttf-jetbrains-mono-nerd papirus-icon-theme noto-fonts noto-fonts-cjk noto-fonts-emoji &&
-yay -S tokyonight-gtk-theme-git &&
+yay -S tokyonight-gtk-theme-git brave-bin vesktop-bin spotify-bin &&
 
 rm -rf $HOME/.config &&
 rm -rf $HOME/.local &&
