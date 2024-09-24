334fc27d1de22eedc5efb08e141eb74eebf06d73
twosich <latriasouls@protonmail.com>
2024-09-24 01:49:40 -0300

    updated things

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 sudo pacman -S git wget curl &&
 git clone https://aur.archlinux.org/yay-bin && cd yay-bin && makepkg -si &&
-sudo pacman -S hyprland hyprshot swaybg swaync lxsession-gtk3 alacritty rofi waybar thunar xdg-desktop-portal-hyprland xdg-desktop-portal-gtk helix && 
-sudo pacman -S ttf-jetbrains-mono-nerd papirus-icon-theme noto-fonts noto-fonts-cjk noto-fonts-emoji &&
-yay -S tokyonight-gtk-theme-git brave-bin vesktop-bin spotify-bin &&
+sudo pacman -S hyprshot swaybg swaync lxsession-gtk3 rofi waybar thunar xdg-desktop-portal-hyprland xdg-desktop-portal-gtk pipewire pipewire-alsa pipewire-pulse wireplumber qpwgraph && 
+sudo pacman -S ttf-jetbrains-mono-nerd papirus-icon-theme noto-fonts noto-fonts-cjk noto-fonts-emoji fastfetch xwaylandvideobridge &&
+yay -S tokyonight-gtk-theme-git brave-bin vesktop-bin spotify-bin hyprland-git wlroots-git alacritty-git audiorelay &&
 
 rm -rf $HOME/.config &&
 rm -rf $HOME/.local &&
